6a76dd7889f29287a12487b6e0f7a47e45c15f8a
Shafujiu <981215707@qq.com>
2020-08-04 19:47:16 +0800

    Initial Commit

---
+++ FlodTableViewCellDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlodTableViewCellDemo
+//
+//  Created by Shafujiu on 2020/8/4.
+//  Copyright © 2020 Shafujiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
